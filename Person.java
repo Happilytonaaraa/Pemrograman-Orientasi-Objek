bc7335202fce203a6afd14017caa0e8b841fbcd7
Nanda Ratna Sari <115931288+Happilytonaaraa@users.noreply.github.com>
2022-10-29 06:59:55 +0700

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,15 @@
+Nama person1 : Anton
+Jenis Kelamin orang pertama : Laki-laki
+Umur orang pertama : 21
+
+**-----------------------------------**
+
+Nama person2 : Riko
+Jenis Kelamin orang Kedua : Laki-laki
+Umur orang Kedua : 20
+
+**-----TERIMA KASIH-----**
+
+
+
+** Process exited - Return Code: 0 **
